2baece7ebe15dc387f3d59f4a3ff756f302a3ff4
Jose Isaac-cura <demon.mystery@gmail.com>
2019-03-08 15:07:30 -0400

    add ability to create complex objects

--- src/instance/test.ts
+++ src/instance/test.ts
@@ -1,22 +1,32 @@
 import { Model } from "../model/model";
+import { type } from "os";
+import { isDate } from "util";
 
 interface testInterface{
     name:string;
+    dat:Date,
     lastname:string;
+}
+
+type inf<T> = {
+    [P in keyof T]
 }
 
 export class Test extends Model{
 
-    protected readonly checkable:Array<keyof testInterface> = [
+    protected readonly _checkable:Array<keyof testInterface> = [
         "name",
         "lastname"
     ];
 
+
     constructor(rawObject = null){
         super(rawObject);
+        this.init();
     }
 
     keys(keys:Array<keyof testInterface>):Array<string>{
         return keys;
     }
+    
 }
